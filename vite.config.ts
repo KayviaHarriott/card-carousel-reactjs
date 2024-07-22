b850ea5d688e2435b97b2171f36439f85973762b
Kayvia Harriott <kayviaharriott@hotmail.com>
2024-07-22 15:07:17 -0500

    Updated package so that card carousel is accessible through export

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/index.ts',           // Your entry point
+      name: 'CardCarouselReactJS',      // Library name
+      fileName: (format) => `card-carousel-reactjs.${format}.js`,  // Output file name
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],  // Specify dependencies to be treated as external
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
